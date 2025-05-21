cafd5fc03eec609c76715d3ff70a922eb40c3eaa
idimmusix <idimmusix@gmail.com>
2025-05-20 21:36:30 -0400

    debug test failure in backend code

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -32,6 +32,7 @@ def client() -> Generator[TestClient, None, None]:
 
 @pytest.fixture(scope="module")
 def superuser_token_headers(client: TestClient) -> dict[str, str]:
+    
     return get_superuser_token_headers(client)
 
 
